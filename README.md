d2968651363b8bc132ca5ff3bf3fc9e50a495fdc
siyavuyachagi <syavuya08@gmail.com>
2025-09-20 17:59:39 +0200

    Basic setup

--- README.md
+++ README.md
@@ -1,13 +1,19 @@
+# Nodejs + TypeScript API
 
-##Setup
+## Setup
 
 1. Install dependancies
 ```bash
-npm install express typescript
-npm install -D @types/express @types/node nodemon
+npm install express typescript dotenv
+npm install -D @types/express @types/node @types/dotenv nodemon rimraf concurrently
 ```
 
 2. Create tsconfig.json file
 ```bash
 npx tsc --init
 ```
+Uncomment the following lines in the file
+    "rootDir": "./src",
+    "outDir": "./dist",
+
+3. Edit scripts
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
 import express, { type Request, type Response } from "express";
-
+import "dotenv/config";
 
 const app = express();
 const port = 5000;
 
 app.get("/", (req: Request, res: Response) => {
-    console.log(req)
-    res.status(200).json("Hello world")
+    res.json("Hello world")
+})
+
+app.get("/health", (req: Request, res: Response) => {
+    res.json({ status: "ok" })
 })
 
 
 app.listen(port, () => {
-    console.log(`App running at http//localhost:${port}`)
+    console.log(`App running at http://localhost:${port}`)
 })
